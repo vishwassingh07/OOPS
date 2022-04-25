f804ba35607be772d6144bb1b1129e8b20b48829
Vishwas <baghel.vishwas0@gmail.com>
2022-04-25 16:06:54 +0530

    [Vishwas]add: Inventory Management Program

---
+++ OOPS/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Inventory Management ");
+        }
+    }
+}
